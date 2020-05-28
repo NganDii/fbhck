1006faefc53f08fcb9e9c302ea9f97dcdfae6bdf
RemApple <32689703+NganDii@users.noreply.github.com>
2020-05-28 09:50:43 +0530

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,3 +1,4 @@
+
 #Install
 cd $HOME
 pkg update -y
@@ -5,7 +6,7 @@ pkg upgrade -y
 pkg install git -y
 pip2 install mechanize
 pkg install python2-dev
-git clone https://github.com/NganDii/fbhck
+
 cd fbhck
 unzip fbhck.zip
 rm fbhck.zip
